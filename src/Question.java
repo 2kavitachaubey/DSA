0f2c455470b061b0f0f305e3a7dc56442fe205de
2kavitachaubey <chaubeykavita750@gmail.com>
2025-01-06 23:47:42 +0530

    About Array

--- src/Question.java
+++ src/Question.java
@@ -2,17 +2,29 @@ import java.util.Arrays;
 
 public class Question {
     public static void main(String [] args ){
-        int arr[] = {1,1,2,2,3,4,5};
-        int[] newArr = new int[arr.length];
-        int index = 0;
+//        int arr[] = {1,1,2,2,3,4,5};
 
-        for(int i=0; i < arr.length-1 ; i++){
-            if(arr[i] != arr[i+1]){
-                newArr[index++] = arr[i];
+        // QUESTION 1
+//        int[] newArr = new int[arr.length];
+//        int index = 0;
+//
+//        for(int i=0; i < arr.length-1 ; i++){
+//            if(arr[i] != arr[i+1]){
+//                newArr[index++] = arr[i];
+//            }
+//        }
+//        newArr[index++] = arr[arr.length-1];
+//        newArr = Arrays.copyOf(newArr,index);
+//        System.out.println(Arrays.toString(newArr));
+
+        //QUESTION 2
+        int arr[] = {1,4,2,7,8,2};
+        int highest = Integer.MIN_VALUE;
+        for(int value: arr){
+            if(value>highest){
+                highest=value;
             }
         }
-        newArr[index++] = arr[arr.length-1];
-        newArr = Arrays.copyOf(newArr,index);
-        System.out.println(Arrays.toString(newArr));
+        System.out.println(highest);
     }
 }
