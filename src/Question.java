57e05bbdbb7ec8d0b4f4bec22998263e6161e064
2kavitachaubey <chaubeykavita750@gmail.com>
2025-01-11 23:42:17 +0530

    About Array

--- src/Question.java
+++ src/Question.java
@@ -74,17 +74,33 @@ public class Question {
 //        System.out.println(count);
 
         // QUESTION 5
-        int target = 9;
-        int[] result = new int[2];
-        for( int i = 0 ; i< arr.length-1; i++){
-            for(int j = i+1; j< arr.length; j++){
-                if((arr[i] + arr[j]) == target){
-                    result[0] = i;
-                    result[1] = j;
-                }
-            }
+//        int target = 9;
+//        int[] result = new int[2];
+//        for( int i = 0 ; i< arr.length-1; i++){
+//            for(int j = i+1; j< arr.length; j++){
+//                if((arr[i] + arr[j]) == target){
+//                    result[0] = i;
+//                    result[1] = j;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(result));
+
+        int x;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        x = sc.nextInt();
+        int r, n = 0, original = x;
+        while(x>0){
+            r = x % 10;
+            n = n * 10 + r;
+            x = x / 10;
         }
-        System.out.println(Arrays.toString(result));
+        if(n == original){
+            System.out.println("It's a palindrome.");
+        }else{
+            System.out.println("It's not a palindrome.");
+        }
 
     }
 }
