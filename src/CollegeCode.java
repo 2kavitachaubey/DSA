7855b6128472c205331d08f57cadc1be2418f4c7
2kavitachaubey <chaubeykavita750@gmail.com>
2025-01-25 23:46:05 +0530

    About Array

--- src/CollegeCode.java
+++ src/CollegeCode.java
@@ -118,21 +118,21 @@ public class CollegeCode {
             System.out.println("It's not a leap year");
         }
 
-        // QUESTION 4....
-        int marks = 75;
-        if(marks >= 90 && marks <= 100){
-            System.out.println("A");
-        }
-        else if(marks >= 80 && marks <= 89){
-            System.out.println("B");
-        }
-        else if(marks >= 70 && marks <= 79){
-            System.out.println("C");
-        }else if(marks >= 60 && marks <= 69){
-            System.out.println("D");
-        }else{
-            System.out.println("F");
-        }
+//        // QUESTION 4....
+//        int marks = 75;
+//        if(marks >= 90 && marks <= 100){
+//            System.out.println("A");
+//        }
+//        else if(marks >= 80 && marks <= 89){
+//            System.out.println("B");
+//        }
+//        else if(marks >= 70 && marks <= 79){
+//            System.out.println("C");
+//        }else if(marks >= 60 && marks <= 69){
+//            System.out.println("D");
+//        }else{
+//            System.out.println("F");
+//        }
 
 
 
