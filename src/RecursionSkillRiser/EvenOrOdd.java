1771ede39716a3cbdaf54734d00fdadf2a916898
2kavitachaubey <chaubeykavita750@gmail.com>
2025-05-30 11:35:19 +0530

    about recursion

--- src/RecursionSkillRiser/EvenOrOdd.java
+++ src/RecursionSkillRiser/EvenOrOdd.java
@@ -1,4 +1,17 @@
 package RecursionSkillRiser;
 
 public class EvenOrOdd {
+    static void count(int num,int even,int odd){
+        if(num == 0){
+            System.out.println("Even count: "+ even);
+            System.out.println("Odd count: "+ odd);
+            return;
+        }
+        if((num%10)%2 == 0) even++;
+        else odd++;
+        count(num/10,even,odd);
+    }
+    public static void main(String[] args) {
+        count(122222,0,0);
+    }
 }
