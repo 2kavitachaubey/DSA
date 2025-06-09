a56d8568edd38e9116e9a358f788ed5855ebe076
2kavitachaubey <chaubeykavita750@gmail.com>
2025-06-09 18:32:26 +0530

    about recursion

--- src/RecursionSkillRiser/MazeProblem.java
+++ src/RecursionSkillRiser/MazeProblem.java
@@ -1,4 +1,18 @@
 package RecursionSkillRiser;
 
 public class MazeProblem {
+    public static void maze(int row, int col, int endrow,int endcol,String result){
+        if(row == endrow && col == endcol){
+            System.out.println(result);
+            return;
+        }
+        if(row > endrow || col > endcol){
+            return;
+        }
+        maze(row,col+1,endrow,endcol,result+"H");
+        maze(row+1,col,endrow,endcol,result+"V");
+    }
+    public static void main(String[] args) {
+        maze(0,0,2,2,"");
+    }
 }
