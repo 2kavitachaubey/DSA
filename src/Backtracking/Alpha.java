3b04b2db05a67f5095e06a69ce1fd52e2a08cbc6
2kavitachaubey <chaubeykavita750@gmail.com>
2025-06-12 13:43:01 +0530

    about recursion

---
+++ src/Backtracking/Alpha.java
@@ -0,0 +1,18 @@
+package Backtracking;
+
+public class Alpha {
+    public static void forAll(int len, String result){
+        if(len == 0){
+            System.out.print(result+ ", ");
+            return;
+        }
+        for(char single = 'A'; single <= 'Z'; single++){
+            if(single == 'A' || single == 'E' || single == 'I'|| single == 'O' || single == 'U'){// Backtracking condition only for what we need.
+                forAll(len-1,result+single);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        forAll(8,"");
+    }
+}
