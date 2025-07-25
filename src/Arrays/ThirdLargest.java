d89bdc72377491d8c4e23126f325fb9e4342beb2
2kavitachaubey <chaubeykavita750@gmail.com>
2025-07-26 00:47:10 +0530

    about recursion

---
+++ src/Arrays/ThirdLargest.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class ThirdLargest {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,5,2,7,9,4};
+        if(arr.length < 3){
+            System.out.println(-1);
+        }
+        int min = -1;
+        int max = -1;
+        int mid = -1;
+        for(int i : arr){
+            if(i > max){
+                min = mid;
+                mid = max;
+                max = i;
+            }else if(i>mid && i != max){
+                min = mid;
+                mid = i;
+            }else if(i>min && i != max && i != mid){
+                min = i;
+            }
+        }
+        System.out.println(min);
+    }
+}
