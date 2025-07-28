bb94d7df2a69b1fffb5bbe4b88b2f91dd0ac2cf9
2kavitachaubey <chaubeykavita750@gmail.com>
2025-07-28 23:27:02 +0530

    about recursion

---
+++ src/leetCode/BuyAndSale.java
@@ -0,0 +1,21 @@
+package leetCode;
+
+public class BuyAndSale {
+    public static void main(String[] args) {
+        int[] arr = new int[]{7,6,4,3,1};
+        int min = 0;
+        for(int i = 0; i<arr.length-1; i++){
+            for(int j = i+1; j< arr.length;j++){
+                if(arr[i] - arr[j] < min){
+                    min = arr[i] - arr[j];
+                }
+            }
+        }
+        if(min < 0){
+            System.out.println(min * -1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
