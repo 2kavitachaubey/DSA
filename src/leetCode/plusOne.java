5cac41664cf1c9e1caedcc9575c5f6207f2401b9
2kavitachaubey <chaubeykavita750@gmail.com>
2025-08-02 23:45:59 +0530

    Update plusOne.java

--- src/leetCode/plusOne.java
+++ src/leetCode/plusOne.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class plusOne {
     public static void main(String[] args) {
-        int [] num = {9,9,9,8};
+        int [] num = {9,10,9,8};
         for(int i = num.length -1; i>0; i--){
             if(num[i] < 9){
                 num[i]++;
