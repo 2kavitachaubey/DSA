476fcb62873d02a971eaf09c8ec9b4922c4dfca5
2kavitachaubey <chaubeykavita750@gmail.com>
2025-08-30 17:32:35 +0530

    about Array

--- src/Arrays/MoveZero.java
+++ src/Arrays/MoveZero.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MoveZero {
     public static void main(String[] args) {
-        int num[] = {0,1,0,3,11};
+        int num[] = {0,1,0,3,12};
         int write = 0;
         for(int read = 0; read<num.length; read++){
             if(num[read]!=0){
