1b371023ae592f5de32e743da6365c3557e58b0a
2kavitachaubey <chaubeykavita750@gmail.com>
2025-10-09 23:50:20 +0530

    about Array

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -1,6 +1,6 @@
 public class PrimeNumber {
     public static void main(String[] args) {
-        int num = 6;
+        int num = 7;
         boolean flag = true;
         for(int i = 2; i<num; i++){
             if(num%i == 0){
